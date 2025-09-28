0de9cf28cea3031d51b7d3fddd2133f53d42a09e
Unaffiliated Entity <unaffiliatedentityllc@gmail.com>
2025-09-28 17:53:15 -0400

    Add global JSX typing for all React Three Fiber primitives

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,8 +3,6 @@ import { ReactThreeFiber } from "@react-three/fiber";
 
 declare global {
   namespace JSX {
-    interface IntrinsicElements extends ReactThreeFiber.IntrinsicElements {
-      mesh: ReactThreeFiber.Object3DNode<THREE.Mesh, typeof THREE.Mesh>;
-    }
+    interface IntrinsicElements extends ReactThreeFiber.IntrinsicElements {}
   }
 }
